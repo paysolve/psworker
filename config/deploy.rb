1d76164303ecc996be47afd525fed01e35f23209
Parris Koumouris <parriskoumouris@Parriss-MacBook-Pro.local>
2024-07-13 21:00:40 +1000

    cap no cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,7 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+
+# set :default_env, { 'RAILS_MASTER_KEY' => File.read('config/master.key').strip }
+
