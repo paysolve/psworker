3d4cd19ffda2f52b022f53f13ac294169ba9f729
Parris Koumouris <parriskoumouris@Parriss-MacBook-Pro.local>
2024-12-19 15:02:40 +1100

    And remove accidental = after posted_datetime

--- db/migrate/20241217102816_add_status_to_account.rb
+++ db/migrate/20241217102816_add_status_to_account.rb
@@ -5,7 +5,7 @@ class AddStatusToAccount < ActiveRecord::Migration[7.0]
     add_index :transfers, :code
     add_index :transfers, :identifier
     add_index :transfers, :posted_date
-    add_index :transfers, :posted_datetime=
+    add_index :transfers, :posted_datetime
     add_index :transfers, :psmain_code
   end
 end
